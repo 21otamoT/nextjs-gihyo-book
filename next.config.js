8e5c5c03f4af491783044cdea90c81e735cbd192
hirose <yukihio76@gmail.com>
2023-12-19 23:36:23 +0900

    スタイルの追加

--- next.config.js
+++ next.config.js
@@ -13,7 +13,16 @@ const nextConfig = {
         reactRemoveProperties: {properties: ['^data-testid$']},
       }
     }
-  })
+    return compilerConfig
+  })(),
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
